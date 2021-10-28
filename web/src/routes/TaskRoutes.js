67620187b2fb09b6f5eeadc684bacabb4b9b640f
crf2a <mdsalan2@gmail.com>
2021-10-28 15:27:12 -0300

    filtros porntos

--- web/src/routes/TaskRoutes.js
+++ web/src/routes/TaskRoutes.js
@@ -5,10 +5,15 @@ const TaskController = require('../controller/TaskController');
 
 router.post('/', TaskController.create);
 router.put('/:id', TaskController.update);
+router.put('/:id/:done', TaskController.done);
 router.get('/filter/all', TaskController.readAll);
 router.get('/:id', TaskController.readById);
 router.delete('/:id', TaskController.delete);
-
+router.get('/filter/late', TaskController.late);
+router.get('/filter/today', TaskController.today);
+router.get('/filter/week', TaskController.week);
+router.get('/filter/month', TaskController.month);
+router.get('/filter/year', TaskController.year);
 
 
 module.exports = router;
